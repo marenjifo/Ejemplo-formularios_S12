32f5d229c4a32990d7403a6a417b864a026c1a9b
Maria Alejandra Renjifo <marialejarenjifo@hotmail.com>
2019-04-12 07:43:59 -0500

    Primera ruta funcionando

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-//Importar librerias
+//1. Importar librerias
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-//Crear app de express
+//1. Crear app de express
 var app = express();
 
-//Establecer la carpeta public como estatica
+//1. Establecer la carpeta public como estatica
 app.use(express.static('public'));
 
-//Registro de handlebars
+//1. Registro de handlebars
 app.engine('handlebars',exphbs());
-//Establecer handlebars como el motor de render
+//1.Establecer handlebars como el motor de render
 app.set('view engine','handlebars');
 
 
+//2. Ruta inicial
+app.get('/',function(request,response){
+
+    var contexto = {
+        titulo: 'Pagina principal'
+    };
+
+    response.render('home',contexto);
+});
 
 
 
 
 
 
-
-//Escuchar desde puerto 3000
+//1. Escuchar desde puerto 3000
 app.listen(3000, function(){
-    
+    console.log('Servidor en el puerto 3000')
 });
